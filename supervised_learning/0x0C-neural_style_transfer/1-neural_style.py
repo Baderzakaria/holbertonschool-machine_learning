e496409d86fcb853015952a5c4d6347f4fd8f733
baderzakaria <albadrrobot@gmail.com>
2021-10-13 16:11:19 +0300

    udpate 0x0C

---
+++ supervised_learning/0x0C-neural_style_transfer/1-neural_style.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+
+if __name__ == '__main__':
+    style_image = mpimg.imread("starry_night.jpg")
+    content_image = mpimg.imread("golden_gate.jpg")
+
+    nst = NST(style_image, content_image)
+    nst.model.summary()
---
+++ supervised_learning/0x0C-neural_style_transfer/3-neural_style.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import matplotlib.image as mpimg
+
+if __name__ == '__main__':
+    style_image = mpimg.imread("starry_night.jpg")
+    content_image = mpimg.imread("golden_gate.jpg")
+
+    nst = NST(style_image, content_image)
+    print(nst.gram_style_features)
+    print(nst.content_feature)
